1f6c85753ccd34c04029721914555f5998cdd0e6
Benjamin Reimer <benreimer9@gmail.com>
2020-03-15 17:34:25 -0400

    text constants

---
+++ src/constants/text.js
@@ -0,0 +1,25 @@
+import * as color from './colors';
+
+export const Title1 = styled.Text`
+    fontSize: 48px;
+    fontWeight: bold;
+    color: ${color.gray700};
+`
+
+export const Title2 = styled.Text`
+    fontSize: 32px;
+    fontWeight: bold;
+    color: ${color.gray700};
+`
+
+export const Title3 = styled.Text`
+    fontSize: 20px;
+    fontWeight: bold;
+    color: ${color.gray700};
+`
+
+export const Title4 = styled.Text`
+    fontSize: 18px;
+    fontWeight: bold;
+    color: ${color.gray700};
+`
